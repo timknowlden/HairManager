04e3f53ba0b50d42e67f4d3bfa6c9065d8fe40de
timknowlden <timknowlden@mattressman.co.uk>
2025-12-19 17:05:04 +0000

    Fix remaining ORDER BY to preserve CSV import order

--- routes/appointments.js
+++ routes/appointments.js
@@ -428,7 +428,7 @@ router.get('/export/csv', (req, res) => {
   const userId = req.userId;
   
   db.all(
-    'SELECT * FROM appointments WHERE user_id = ? ORDER BY date DESC, id DESC',
+    'SELECT * FROM appointments WHERE user_id = ? ORDER BY date DESC, id ASC',
     [userId],
     (err, rows) => {
       if (err) {
